81df0723389b6a1f63d44021de344a08855665a8
Carlos Medina C <carlos.medina@arandasoft.com>
2023-10-20 16:22:21 -0500

    Re ordenar directorios

---
+++ decoder/decoder.asm
@@ -0,0 +1,40 @@
+.data 
+decodedMessageBuffer: .space 1024 
+
+.text
+
+.globl decodedMessage
+      
+# Funcion para codificar un mensaje
+
+decodedMessage:
+	move $t0, $a0		# Param1: Buffer que contiene el mensaje a decodificar
+	move $t1, $s0		# Diccionario temporal 		
+	# Registros
+	la $s4, decodedMessageBuffer
+	# Loop mensaje
+	loop_main:
+            lb $t2, ($t0)  	   # message[i]
+            li $t3, 0		   # index = 0    		      
+            
+            # Loop diccionario
+			loop_child:    	
+        		lb $t4, ($t1)     
+        		blt $t3, $t2, increase_index # if( index < message[i].value )
+        		increase_index:
+        			addi $t1, $t1, 1   # avanza una posicion puntero diccionario[i + 1]
+        			j loop_child
+        		beq $t3, $t2, value_found:
+        		value_found:
+        			lb $t5, ($t1) # $t5 = diccionario[index]
+        			sb $t5, ($s4)  # Almacenar el valor de $t5 en la ubicación actual de $s4
+    				addiu $s4, $s4, 4
+        		j end_loop_child
+    		end_loop_child:
+
+    		continue_main_loop:
+
+			beqz $t2, end_loop_main    			     
+          	j loop_main           	# Volver al inicio del bucle principal
+    end_loop_main:
+    jal returnToMain
--- encoder/main.asm
+++ encoder/main.asm
@@ -1,5 +1,4 @@
 .data
-	debug_mode: 	  .word 0   # Variable global para indicar el modo de depuración (1 para habilitado, 0 para deshabilitado)
 	digramFileName:   .asciiz "dictionary.txt"  	# Nombre del archivo que contiene el diccionario
 	digramBuffer:     .space  1024  			# Buffer para el diccionario
 	inputFileName:    .asciiz "digram_test.java"	# Nombre del archivo a codificar
@@ -12,8 +11,10 @@
 .globl main
 .globl returnToMain
 main:
-	lw $s7, debug_mode         # Cargar el valor de debug_mode en $s7
+	#####
+	# TAMANO DEL DICCIONARIO
 	li $s2, 7
+	#####
 	
 	la	$a0, digramFileName	
 	la	$a1, digramBuffer
@@ -36,37 +37,15 @@ main:
     li $v0, 16               # Código del sistema para cerrar el archivo
     # $a0 <- proviene el descriptor, por ello no se especifica
     syscall
-
-    # Imprimir el contenido leído si el modo depuracion esta habilitado
-    ################ DEBUG ###############
-    move $a0, $s0
-    beq  $s7, $zero, notDebug
-    jal debug
-    notDebug:
-    ################ DEBUG ###############
-    
+  
     move $a0, $s1
     jal encondedMessage
     
     returnToMain:
     
-    #### TODO: 
-    # Obtener $s3 -> Contenido codificado
-    # Escribir valor $s3 en output.txt
-    
-    
     move $a0, $s3
-    jal print_buffer
-    #la	 $a0, ouputFileName	
-	#move $a1, $s3
-    #jal writeFile
+    jal saveEncodedMessage
 
-	
-	
-  	#la	 $a0, ouputFileName
-    #la   $a1, ouputFileBuffer
-    #jal  openFile
-    
     move $s3, $v0				# Obtener buffer resultante de $v0. $s1 -> Contiene direccion del archivo a codificar
 
 end_program:
@@ -77,7 +56,7 @@ end_program:
 
 # Función para imprimir un buffer hasta encontrar un número negativo
 # $a0: Dirección base del buffer
-print_buffer:
+saveEncodedMessage:
 	move $t9, $a0
 	la   $t6, ouputFileBuffer 
     
@@ -88,13 +67,13 @@ print_buffer:
     # $t6: Buffer de salida archivo output.txt
     
     # Abrir el archivo para escribir        
-    la $a0, ouputFileName  		 # Dirección de la cadena que contiene el nombre del archivo
+    la $a0, ouputFileName  		 
     jal openWriteFile
     
     move $s7, $v0 # Almacenar descriptor
-loop_print_buffer:
+loop_save_encoded_message:
+
     lb $t8, ($t9)  # Almacena elemento int
-    
     move   $a0, $t8
     
     # Llamar a la función itoa para convertir el entero en una cadena ASCII
@@ -102,7 +81,7 @@ loop_print_buffer:
 
 	move $t7, $v0 # Pasar resultado conversion a $t7
 	
-    bltz $t8, end_print_buffer  # Salir del bucle si el elemento es negativo
+    bltz $t8, end_loop_save_encoded_message  # Salir del bucle si el elemento es negativo
 
 	# Escribir en el archivo
     move $a0, $s7        # File descriptor devuelto por la llamada a abrir
@@ -110,11 +89,9 @@ loop_print_buffer:
     jal writeFile
     
     addiu $t9, $t9, 4  # Avanzar al siguiente elemento en el buffer
-    j loop_print_buffer
+    j loop_save_encoded_message
 
-end_print_buffer: 
- 	# Cerrar el archivo
- 	
+end_loop_save_encoded_message: 
     move $a0, $s7        # File descriptor devuelto por la llamada a abrir
     jal closeWriteFile
       
